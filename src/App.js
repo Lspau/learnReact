eeefb7975cab80034c098843015a7927ef1cc7b8
SoLy <lspau95@gmail.com>
2023-09-04 14:46:46 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { ToastContainer, toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+const {test} = useSelector( state => state.app)
+console.log(test);
   return (
-    <div className="App">
-     app
-    </div>
+    <>
+      <div className="">
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
   );
 }
 
